2ef53e11a567101e473321422125ded3dc0be65b
Slawomir Borowiec <przodownik@tlen.pl>
2016-05-13 10:55:45 +0200

    adoc updated

--- src/test/java/pl/java/scalatech/constructorResult/ConstructorResultTest.java
+++ src/test/java/pl/java/scalatech/constructorResult/ConstructorResultTest.java
@@ -2,6 +2,12 @@ package pl.java.scalatech.constructorResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +16,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.extern.slf4j.Slf4j;
 import pl.java.scalatech.config.TestJpaConfig;
+import pl.java.scalatech.domain.constructorResult.Todo;
+import pl.java.scalatech.pojo.TodoDTO;
 import pl.java.scalatech.repository.constructorResult.ConstructorResultRepo;
 
 @Slf4j
@@ -18,10 +26,21 @@ import pl.java.scalatech.repository.constructorResult.ConstructorResultRepo;
 public class ConstructorResultTest {
     @Autowired
     private ConstructorResultRepo repo;
+    @Autowired
+    private EntityManager em;
 
     @Test
-    public void shouldSave(){
+    public void shouldSaveAndThanFind(){
         assertThat(repo).isNotNull();
+        repo.save(new Todo("all", "java"));
+        repo.save(new Todo("all", "jsp"));
+        repo.save(new Todo("my", "maven"));
+        repo.save(new Todo("you", "gradle"));
+        Query query = em.createNamedQuery("findWithTodoResultSetMapper",TodoDTO.class);
+        query.setParameter(1, "ja%");
+        List<TodoDTO> result =  query.getResultList();
+        log.info("{}",result);
+        Assertions.assertThat(result).hasSize(1);
     }
 
 }
